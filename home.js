8bb3230e885957d7a13f23d1a3ddd3ab67b1724f
universeoforanges <999nooberplays@gmail.com>
2023-06-20 23:42:17 -0500

    inital commit

---
+++ home.js
@@ -0,0 +1,33 @@
+var randomText = document.getElementById("random");
+
+let randomVal = Math.floor(Math.random() * 9);
+if (randomVal == 0) {
+    randomText.innerHTML = "Lovely wannabes :/";
+}
+else if (randomVal == 1) {
+    randomText.innerHTML = "Yo i died dont take my stuff plz";
+}
+else if (randomVal == 2) {
+    randomText.innerHTML = "?kick universeoforanges He said my house sucks";
+}
+else if (randomVal == 3) {
+    randomText.innerHTML = "?ban universeoforanges major grief";
+}
+else if (randomVal == 4) {
+    randomText.innerHTML = "YO WHY DID YOU DESTROY THE BASE NOW WHERE WE GONNA LIVE";
+}
+else if (randomVal == 5) {
+    randomText.innerHTML = "this kid sucks";
+}
+else if (randomVal == 6) {
+    randomText.innerHTML = "BRO I DIED AGAIN THIS GAME SUCKS";
+}
+else if (randomVal == 7) {
+    randomText.innerHTML = "(Guy): this build is trash, try harder <br> (superduper990000): ?ban guy mean to owner";
+}
+else if (randomVal == 8) {
+    randomText.innerHTML = "XD LOL IM DUM";
+}
+else if (randomVal == 9) {
+    randomText.innerHTML = "Abstractverse abstractverse{}; <br>  abstractverse.getSteam() <br> error: getSteam was undefined. did you mean `getALife()?`"
+}
