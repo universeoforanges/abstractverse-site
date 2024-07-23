50ae41b6a8ab2ed7be0c04c48b4133178b6003bd
universeoforanges <999nooberplays@gmail.com>
2024-07-23 12:33:23 -0500

    normal site changes and satan's blood hot sauce

--- home.js
+++ home.js
@@ -6,9 +6,20 @@ var messages = [
     "YO WHY DID YOU DESTROY THE BASE NOW WHERE WE GONNA LIVE",
     "this kid sucks",
     "BRO I DIED AGAIN THIS GAME SUCKS",
-    "(Guy): this build is trash, try harder <br> (superduper990000): ?ban guy mean to owner",
+    "Guy: this build is trash, try harder <br> superduper990000: ?ban guy mean to owner",
     "XD LOL IM DUM",
-    "Abstractverse abstractverse{}; <br>  abstractverse.getSteam() <br> error: getSteam is undefined. did you mean `getALife()?`"
+    "Abstractverse abstractverse{}; <br>  abstractverse.getSteam(); <br> error: getSteam is undefined. did you mean `getALife()`?",
+    "I farted",
+    "always remember that caseoh's head is built like a circle",
+    "Sniped from 500 yards",
+    "Now minecraft PE edition!",
+    "K,",
+    "An error occured loading this message.",
+    "whitematic",
+    "Big Mac Trouble!",
+    "Switch droid",
+    "patch release 32432.69028.93920: added farting to the game",
+    "THIS IS AN INCREDIBLY HARD WORD"
 ]
 
 var randomText = document.getElementById("random");
